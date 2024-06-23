d11cb544df560b4f234bf4c9f27623583e9dc140
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-23 09:52:55 +0500

    Form checked

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -10,57 +10,81 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { useState } from 'react';
 
 export default function RegisterPage() {
-  const handlerSubmit = (e)=>{
+  const [inputValues, setInputValues] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputValues(values => ({ ...values, [name]: value }))
+  }
+
+  const handlerSubmit = (e) => {
     e.preventDefault();
-    console.log("submit")
+    setInputValues(inputValues);
+    console.log(inputValues);
   }
   return (
-   <div className ="h-screen flex justify-center items-center  ">
-     <Card className="w-full max-w-sm ">
-      <CardHeader>
-        <CardTitle className="text-xl">Sign Up</CardTitle>
-        <CardDescription>
-          Enter your information to create an account
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
+    <div className="h-screen flex justify-center items-center  ">
+      <Card className="w-full max-w-sm ">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
 
-      <form onSubmit={handlerSubmit }>
+          <form onSubmit={handlerSubmit}>
 
-      <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="full-name">Full Name</Label>
-            <Input id="full-name" placeholder="Full Name" required />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password"  placeholder="******" type="password" />
-          </div>
-          <Button type="submit" className="w-full">
-            Create an account
-          </Button>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="full-name">Full Name</Label>
+                <Input id="full-name"
+                  placeholder="Full Name"
+                  required
+                  name="name"
+                  value={inputValues.name || ""}
+                  onChange={handleChange} />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                  name="email"
+                  value={inputValues.email || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password"
+                  placeholder="******"
+                  type="password"
+                  name="password"
+                  value={inputValues.password || ""}
+                  onChange={handleChange}
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Create an account
+              </Button>
 
-        </div>
-      </form>
-        <div className="mt-4 text-sm text-center">
-          Already have an account?{" "}
-          <Link href="#" className="underline">
-            Sign in
-          </Link>
-        </div>
-      </CardContent>
-    </Card>
-   </div>
+            </div>
+          </form>
+          <div className="mt-4 text-sm text-center">
+            Already have an account?{" "}
+            <Link href="#" className="underline">
+              Sign in
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
   )
 }
