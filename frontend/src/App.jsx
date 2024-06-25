4a1d266c90f8029b985682ac3d7c043825a6ef62
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-25 21:52:54 +0500

    order

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import HomePage from "./pages/HomePage";
 import DashboardLayout from "./pages/Admin/DashboardLayout";
 import Dashboard from "./pages/Admin/Dashboard";
 import Users from "./pages/Admin/Users";
+import Products from "./pages/Admin/Products";
+import Orders from "./pages/Admin/Orders";
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/admin" element={<DashboardLayout />}>
             <Route path="dashboard" element={<Dashboard />} />
             <Route path="users" element={<Users />} />
+            <Route path="products" element={<Products/>} />
+            <Route path="orders" element={<Orders/>} />
           </Route>
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/login" element={<LoginPage />} />
