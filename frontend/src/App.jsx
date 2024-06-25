009a1c61c61edd8f59cf813eb7ecf1d13fd6c288
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-25 19:09:08 +0500

    Dashboard Layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,14 @@ import RegisterPage from "./pages/RegisterPage";
 import { ToastContainer } from 'react-toastify';
 import  LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
-import Dashboard from "./pages/Admin/Dashboard";
+import DashboardLayout from "./pages/Admin/DashboardLayout.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/admin/dashboard" element={<Dashboard />} />
+      <Route path="/admin/dashboard" element={<DashboardLayout />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
