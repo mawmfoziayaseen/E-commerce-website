cedecfe7085ebc1599ba82924f1f4ace71b1bc3f
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-26 09:15:26 +0500

    get users

--- frontend/src/pages/Admin/Users.jsx
+++ frontend/src/pages/Admin/Users.jsx
@@ -1,27 +1,43 @@
-import { Button } from "@/components/ui/button"
-
+import axios from "axios";
+import { useEffect } from "react";
 
 function Users() {
-    return (
-        <>
-            <div className="flex items-center">
-                <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
-            </div>
-            <div
-                className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-            >
-                <div className="flex flex-col items-center gap-1 text-center">
-                    <h3 className="text-2xl font-bold tracking-tight">
-                        You have no products
-                    </h3>
-                    <p className="text-sm text-muted-foreground">
-                        You can start selling as soon as you add a product.
-                    </p>
-                    <Button className="mt-4">Users</Button>
-                </div>
-            </div>
-        </>
-    )
-}
+
+    const getAllUsers = () => {
+
+        axios
+            .get(`${import.meta.env.VITE_BASE_URL}/users/all-users`, {
+                withCredentials: true,   //axios send automatically cookies when we apply this property
+                headers: { "Content-Type": "application/json" },
+            })
+            .then((response) => {
+                console.log(response.data);
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    };
+    useEffect(() => {
+        getAllUsers();
+    }, []);
+
+
+return (
+    <>
+        <div className="flex items-center">
+            <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
+        </div>
+        <div
+            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+            x-chunk="dashboard-02-chunk-1"
+        >
+            <div className="flex flex-col items-center gap-1 text-center">
+               
+               </div>
+        </div>
+    </>
+);
+};
 
 export default Users;
