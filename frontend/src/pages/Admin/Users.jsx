8bdfbabb4a3f200c0ceb3c3cbf17c352a84a2614
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-26 09:25:30 +0500

    done

--- frontend/src/pages/Admin/Users.jsx
+++ frontend/src/pages/Admin/Users.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 import { useEffect } from "react";
-
+import { useState } from "react";
 function Users() {
+    const [users, setUsers] = useState();
 
     const getAllUsers = () => {
 
@@ -11,7 +12,8 @@ function Users() {
                 headers: { "Content-Type": "application/json" },
             })
             .then((response) => {
-                console.log(response.data);
+                console.log(response);
+                setUsers(response.data);
 
             })
             .catch((error) => {
@@ -23,21 +25,23 @@ function Users() {
     }, []);
 
 
-return (
-    <>
-        <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
-        </div>
-        <div
-            className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
-            x-chunk="dashboard-02-chunk-1"
-        >
-            <div className="flex flex-col items-center gap-1 text-center">
-               
-               </div>
-        </div>
-    </>
-);
+    return (
+        <>
+            <div className="flex items-center">
+                <h1 className="text-lg font-semibold md:text-2xl">Users</h1>
+            </div>
+            <div
+                className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm"
+                x-chunk="dashboard-02-chunk-1"
+            >
+                <div className="flex flex-col items-center gap-1 text-center">
+                    {JSON.stringify(users)};
+
+
+                </div>
+            </div>
+        </>
+    );
 };
 
 export default Users;
