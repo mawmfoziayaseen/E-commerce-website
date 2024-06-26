336cc0480bd6245a49c22a205ee1511c75797283
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-26 12:26:40 +0500

    Admin user

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,18 +1,20 @@
 import express from 'express';
-import { registerController,loginController ,logoutController,allUsersController} from '../controllers/userController.js';
+import { registerController, loginController, logoutController, allUsersController } from '../controllers/userController.js';
+import { isAdmin, isAuthorized } from '../middlewaves/authMiddleware.js';
+
 
 
 const userRouter = express.Router();
 
 // http://localhost:8080/api/v1/users/register
-userRouter .post("/register",registerController);
-userRouter .post("/login",loginController)
-userRouter .get("/logout",logoutController)
+userRouter.post("/register", registerController);
+userRouter.post("/login", loginController)
+userRouter.get("/logout", logoutController)
 // Admin Routes
-userRouter .get("/all-users",allUsersController)
+userRouter.get("/all-users", isAuthorized, isAdmin, allUsersController)
 
 
 
 
 
-export default userRouter ;
+export default userRouter;
