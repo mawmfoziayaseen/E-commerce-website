f0fc3ee53371292bd839495a9393bc3ab4d38aac
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-06-29 00:02:23 +0500

    done

--- frontend/src/pages/Admin/Categories.jsx
+++ frontend/src/pages/Admin/Categories.jsx
@@ -1,12 +1,33 @@
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { AddCategory } from "@/store/features/Categories/CategoriesSlice";
 import { toast } from "react-toastify";
-
-
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
 
 function Categories() {
   const [inputValues, setInputValues] = useState({});
@@ -15,58 +36,250 @@ function Categories() {
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    setInputValues(values => ({ ...values, [name]: value }));
+    setInputValues((values) => ({ ...values, [name]: value }));
   };
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(inputValues);
     dispatch(AddCategory(inputValues))
-        .unwrap()
-        .then((response) => {
-            if (response?.success == true) {
-
-                toast.success(response?.message, { autoClose: 2000 });
-              
-            } else {
-                toast.error(response?.message, { autoClose: 2000 });
-
-            }
-
-        })
-        .catch((error) => {
-            toast.error(error, { autoClose: 2000 });
-        })
-
-
+      .unwrap()
+      .then((response) => {
+        if (response?.success == true) {
+          toast.success(response?.message, { autoClose: 2000 });
+        } else {
+          toast.error(response?.message, { autoClose: 2000 });
+        }
+      })
+      .catch((error) => {
+        toast.error(error, { autoClose: 2000 });
+      });
   };
   return (
     <>
-      <Card>
-        <CardHeader>
-          <CardTitle>Add Categories </CardTitle>
-          <CardDescription>
-            Used to identify your store in the marketplace.
-          </CardDescription>
-        </CardHeader>
-        <CardContent>
-          <form onSubmit={handleSubmit}>
-            <div className="flex">
-              <Input id="name"
-                type="text"
-                placeholder="categories Name"
-                required
-                name="name"
-                value={inputValues.name || ""}
-                onChange={handleChange}
-              />
-              <Button>Add Categories</Button>
-            </div>
-          </form>
-        </CardContent>
-    
-      </Card>
+      <div>
+        <Card>
+          <CardHeader>
+            <CardTitle>Add Categories </CardTitle>
+            <CardDescription>
+              Used to identify your store in the marketplace.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form onSubmit={handleSubmit}>
+              <div className="flex">
+                <Input
+                  id="name"
+                  type="text"
+                  placeholder="categories Name"
+                  required
+                  name="name"
+                  value={inputValues.name || ""}
+                  onChange={handleChange}
+                />
+                <Button>Add Categories</Button>
+              </div>
+            </form>
+          </CardContent>
+        </Card>
+      </div>
+      <section>
+        <Table>
+          <TableHeader>
+            <TableRow>
+            
+              <TableHead>Sr.#</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Slug</TableHead>
+              <TableHead>Date Added</TableHead>
+              <TableHead>Action</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="font-medium">Category Number</TableCell>
+              <TableCell className="font-medium">Category Name</TableCell>
+              <TableCell className="font-medium">Category Slug</TableCell>
+              <TableCell className="font-medium">Category Date</TableCell>
+
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+         
+              </TableCell>
+              <TableCell className="font-medium">
+                Hypernova Headphones
+              </TableCell>
+              <TableCell>
+                <Badge variant="outline">Active</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$129.99</TableCell>
+              <TableCell className="hidden md:table-cell">100</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2023-10-18 03:21 PM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+           
+              </TableCell>
+              <TableCell className="font-medium">AeroGlow Desk Lamp</TableCell>
+              <TableCell>
+                <Badge variant="outline">Active</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$39.99</TableCell>
+              <TableCell className="hidden md:table-cell">50</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2023-11-29 08:15 AM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+           
+              </TableCell>
+              <TableCell className="font-medium">
+                TechTonic Energy Drink
+              </TableCell>
+              <TableCell>
+                <Badge variant="secondary">Draft</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$2.99</TableCell>
+              <TableCell className="hidden md:table-cell">0</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2023-12-25 11:59 PM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+                {/* <Image
+                  alt="Product image"
+                  className="aspect-square rounded-md object-cover"
+                  height="64"
+                  src="/placeholder.svg"
+                  width="64"
+                /> */}
+              </TableCell>
+              <TableCell className="font-medium">
+                Gamer Gear Pro Controller
+              </TableCell>
+              <TableCell>
+                <Badge variant="outline">Active</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$59.99</TableCell>
+              <TableCell className="hidden md:table-cell">75</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2024-01-01 12:00 AM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+           
+              </TableCell>
+              <TableCell className="font-medium">Luminous VR Headset</TableCell>
+              <TableCell>
+                <Badge variant="outline">Active</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$199.99</TableCell>
+              <TableCell className="hidden md:table-cell">30</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2024-02-14 02:14 PM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </section>
     </>
-  )
+  );
 }
 
 export default Categories;
