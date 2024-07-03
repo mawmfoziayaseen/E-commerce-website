fa6ef8284cdcb00c6968ec24867f81ee9c57caa9
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-07-03 11:57:09 +0500

    processing

--- frontend/src/pages/Admin/AddProduct.jsx
+++ frontend/src/pages/Admin/AddProduct.jsx
@@ -1,9 +1,76 @@
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { toast } from "react-toastify";
 
+import { useDispatch, useSelector } from "react-redux";
+import { register } from "@/store/features/auth/authSlice";
 
 function AddProduct() {
+  const [inputValues, setInputValues] = useState({});
+  const { status } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputValues((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+  };
+
   return (
-    <div>AddProduct</div>
-  )
+    <>
+      <Card className="w-full max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Enter your information to create an account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit}>
+            <div className="grid gap-4">
+              <div className="grid gap-2">
+                <Label htmlFor="full-name">Full Name</Label>
+                <Input
+                  id="full-name"
+                  placeholder="Full Name"
+                  required
+                  name="name"
+                  value={inputValues.name || ""}
+                  onChange={handleChange}
+                />
+              </div>
+            
+              <Button
+                type="submit"
+                className="w-full"
+                disabled={status == "loading" ? true : false}
+              >
+                {status == "loading"
+                  ? "Creating Account ....."
+                  : "Create Account"}
+              </Button>
+            </div>
+          </form>
+         
+        </CardContent>
+      </Card>
+    </>
+  );
 }
 
-export default AddProduct
+export default AddProduct;
--- frontend/src/pages/Admin/Products.jsx
+++ frontend/src/pages/Admin/Products.jsx
@@ -1,25 +1,16 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 
 function Products() {
     return (
         <>
-            <div className="flex items-center">
-                <h1 className="text-lg font-semibold md:text-2xl">Products</h1>
-            </div>
-            <div
-                className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm" x-chunk="dashboard-02-chunk-1"
-            >
-                <div className="flex flex-col items-center gap-1 text-center">
-                    <h3 className="text-2xl font-bold tracking-tight">
-                        You have no products
-                    </h3>
-                    <p className="text-sm text-muted-foreground">
-                        You can start selling as soon as you add a product.
-                    </p>
-                    <Button className="mt-4">Products</Button>
-                </div>
-            </div>
+         <div className="flex justify-between items-center">
+        <h2 className="font-semibold text-2xl">Product</h2>
+        <Link to="/admin/products/add">
+        <Button>Add Product</Button>
+        </Link>
+     </div>
         </>
     )
 }
