1cbf693a646f10ecc729a16162d31e78a6418ab6
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-07-03 11:18:18 +0500

    images uploading successfully

--- Backend/middlewares/multerMiddleware.js
+++ Backend/middlewares/multerMiddleware.js
@@ -1,8 +1,6 @@
-import multer from 'multer';
-import {v4 as uuidv4} from 'uuid';
-import path from 'path';
-
-
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
