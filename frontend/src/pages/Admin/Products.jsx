f51650a5ffa474fcddfcc81d8f51fb13c6dc7103
mawmfoziayaseen <foziayaseen8054@outlook.com>
2024-07-04 11:43:02 +0500

    table done

--- frontend/src/pages/Admin/Products.jsx
+++ frontend/src/pages/Admin/Products.jsx
@@ -1,18 +1,134 @@
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Image, MoreHorizontal } from "lucide-react";
+
 import { Link } from "react-router-dom";
 
-
 function Products() {
-    return (
-        <>
-         <div className="flex justify-between items-center">
+  return (
+    <>
+      <div className="flex justify-between items-center">
         <h2 className="font-semibold text-2xl">Product</h2>
         <Link to="/admin/products/add">
-        <Button>Add Product</Button>
+          <Button>Add Product</Button>
         </Link>
-     </div>
-        </>
-    )
+      </div>
+      <div>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Image</TableHead>
+              <TableHead>Title</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Added By</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead>Action</TableHead>
+              
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell className="hidden sm:table-cell">
+                <Image
+                  alt="Product image"
+                  className="aspect-square rounded-md object-cover"
+                  height="64"
+                  src="/placeholder.svg"
+                  width="64"
+                />
+              </TableCell>
+              <TableCell className="font-medium">
+                Laser Lemonade Machine
+              </TableCell>
+              <TableCell>
+                <Badge variant="outline">Draft</Badge>
+              </TableCell>
+              <TableCell className="hidden md:table-cell">$499.99</TableCell>
+              <TableCell className="hidden md:table-cell">25</TableCell>
+              <TableCell className="hidden md:table-cell">
+                2023-07-12 10:42 AM
+              </TableCell>
+              <TableCell>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button aria-haspopup="true" size="icon" variant="ghost">
+                      <MoreHorizontal className="h-4 w-4" />
+                      <span className="sr-only">Toggle menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                    <DropdownMenuItem>Edit</DropdownMenuItem>
+                    <DropdownMenuItem>Delete</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </TableCell>
+            </TableRow>
+            {/* <TableRow>
+                  <TableCell className="hidden sm:table-cell">
+                    <Image
+                      alt="Product image"
+                      className="aspect-square rounded-md object-cover"
+                      height="64"
+                      src="/placeholder.svg"
+                      width="64"
+                    />
+                  </TableCell>
+                  <TableCell className="font-medium">
+                    Hypernova Headphones
+                  </TableCell>
+                  <TableCell>
+                    <Badge variant="outline">Active</Badge>
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    $129.99
+                  </TableCell>
+                  <TableCell className="hidden md:table-cell">100</TableCell>
+                  <TableCell className="hidden md:table-cell">
+                    2023-10-18 03:21 PM
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button
+                          aria-haspopup="true"
+                          size="icon"
+                          variant="ghost"
+                        >
+                          <MoreHorizontal className="h-4 w-4" />
+                          <span className="sr-only">Toggle menu</span>
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent align="end">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem>Edit</DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
+                </TableRow> */}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );
 }
 
 export default Products;
